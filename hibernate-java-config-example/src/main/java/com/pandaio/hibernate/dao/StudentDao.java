5aa357df3128a0e26f853b4ddf4276c7591510e8
Santosh Panda <pandaprogramminghub@gmail.com>
2020-05-16 13:21:12 +0530

    hibernate-java-config-example initial commit

---
+++ hibernate-java-config-example/src/main/java/com/pandaio/hibernate/dao/StudentDao.java
@@ -0,0 +1,47 @@
+package com.pandaio.hibernate.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.pandaio.hibernate.entity.Student;
+import com.pandaio.hibernate.util.HibernateUtil;
+
+public class StudentDao {
+	public void saveStudent(Student student) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// save the student object
+			session.save(student);
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	public List<Student> getStudents() {
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			return session.createQuery("from Student", Student.class).list();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public Student getStudent(int id) {
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			Student student = session.get(Student.class, id);
+			return student;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
